f10fff98f5c8fd195ee36da8b00c0bceb731a861
sainath Shinde <sainathshinde85@gmail.com>
2018-11-02 08:20:33 +0530

    added test program

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("You can do it Sainath ...Keep it up");
+		for(int i=0;i<10;i++){
+			System.out.println("i="+i);
+		}
+	}
+
+}
